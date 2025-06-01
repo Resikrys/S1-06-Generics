4a083e9216ec064ca6d51d9e20efcebbdccc97f1
resikrys <resicris@gmail.com>
2025-06-01 22:32:24 +0200

    add Person class

---
+++ src/s1_06_generics_lvl1_ex1/Person.java
@@ -0,0 +1,18 @@
+package s1_06_generics_lvl1_ex1;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surname + ", " + age + " years.";
+    }
+}
