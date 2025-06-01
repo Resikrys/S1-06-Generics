db617d3dd8f65757c69f1b328801223ceedf170d
resikrys <resicris@gmail.com>
2025-06-01 22:42:14 +0200

    add main class at task6

--- src/s1_06_generics_lvl1_ex1/Task6.java
+++ src/s1_06_generics_lvl1_ex1/Task6.java
@@ -1,5 +1,13 @@
 package s1_06_generics_lvl1_ex1;
 
 public class Task6 {
+    public static void main(String[] args) {
+        Person person1 = new Person("Liam", "Vittori", 11);
+        String text = "Hello Universe";
+        int num = 42;
 
+        GenericMethods.printArguments(person1, text, num);
+        GenericMethods.printArguments(num, person1, text);
+        GenericMethods.printArguments(text, num, person1);
+    }
 }
