c6dac4c3a71e7cd120cc969196019a86c05a664c
resikrys <resicris@gmail.com>
2025-06-01 22:52:03 +0200

    upd genericMethods class

--- src/s1_06_generics_lvl2/GenericMethods.java
+++ src/s1_06_generics_lvl2/GenericMethods.java
@@ -1,9 +1,9 @@
 package s1_06_generics_lvl2;
 
 public class GenericMethods {
-    public static <T, U, V> void printArguments(T arg1, U arg2, V arg3) {
+    public static <T, U> void printArguments(T arg1, U arg2, Person person1) {
         System.out.println(arg1);
         System.out.println(arg2);
-        System.out.println(arg3);
+        System.out.println(person1);
     }
 }
