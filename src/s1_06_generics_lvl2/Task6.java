e7fc67f6bc59575f41007fabd110cd80ed17c86c
resikrys <resicris@gmail.com>
2025-06-01 22:56:03 +0200

    upd main code lvl2 task6

--- src/s1_06_generics_lvl2/Task6.java
+++ src/s1_06_generics_lvl2/Task6.java
@@ -1,16 +1,11 @@
 package s1_06_generics_lvl2;
 
-import s1_06_generics_lvl1_ex2.GenericMethods;
-import s1_06_generics_lvl1_ex2.Person;
-
 public class Task6 {
     public static void main(String[] args) {
-        s1_06_generics_lvl1_ex2.Person person1 = new Person("Liam", "Vittori", 11);
+        Person person1 = new Person("Liam", "Vittori", 11);
         String text = "Hello Universe";
-        int num = 42;
+        double pi = 3.14;
 
-        s1_06_generics_lvl1_ex2.GenericMethods.printArguments(person1, text, num);
-        s1_06_generics_lvl1_ex2.GenericMethods.printArguments(num, person1, text);
-        GenericMethods.printArguments(text, num, person1);
+        GenericMethods.printArguments(text, pi, person1);
     }
 }
